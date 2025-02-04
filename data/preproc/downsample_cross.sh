173f8703760244be1c9284dc980b2e6f403c6c0c
SHMoon <msh2044@gmail.com>
2025-02-04 18:12:02 +0900

    add downsample code and merge df code

---
+++ data/preproc/downsample_cross.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+input_dir="/NFS/FutureBrainGen/data/cross/img"
+output_dir="/NFS/FutureBrainGen/data/cross/down_img_1.7mm"
+
+mkdir -p "$output_dir"
+
+for input_file in "$input_dir"/*.nii; do
+    base_name=$(basename "$input_file" .nii)
+    
+    output_file="$output_dir/${base_name}.nii"
+    
+    flirt -in "$input_file" -ref "$input_file" -applyisoxfm 1.7 -out "$output_file"
+    
+done
