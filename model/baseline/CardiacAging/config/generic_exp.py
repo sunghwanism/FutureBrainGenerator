2aa1db65c17303c832e27c4e4162f0b2d7d28109
HogeonKim <hoho9337@khu.ac.kr>
2025-02-13 18:53:35 +0900

    changed hyperparameters

--- model/baseline/CardiacAging/config/generic_exp.py
+++ model/baseline/CardiacAging/config/generic_exp.py
@@ -1,10 +1,10 @@
 '''Configuration template for a generic experiment'''
-LATENT_DIM = 256 # 130
+LATENT_DIM = 320 # 130
 SHAPE = (86,106,86) # (256,256,256)
 
 EXPERIMENT_PARAMS = {
     # 'augment': True,  # Use data augmentation
-    'batch_size': 3,
+    'batch_size': 2,
     'beta1': 0.9, # first moment decay
     'beta2': 0.999, # second moment decay
     'cycle_cons_weight': 1, # Cycle consistency weight in paper
@@ -14,7 +14,7 @@ EXPERIMENT_PARAMS = {
     'input_shape': SHAPE, # Modified by HG (original : (256,256))
     'kernel_size': (3, 3, 3),
     'latent_space_dim': LATENT_DIM,
-    'learning_rate': 1e-5,  # Learning rate
+    'learning_rate': 1e-4,  # Learning rate
     'module_name': 'GAN',
     'ncritic': 5, # Number of training critic (Discriminator updates ncritic*10 times per Generator update while warmup)
     'num_classes': 1, # used in deprecated TransformerXia
@@ -33,12 +33,12 @@ EXPERIMENT_PARAMS = {
     'use_wclip': False, # not used
     'view': '',
     'nonlinearity': 'relu', # new added by HK
-    'warming_epochs': 10,
-    'weight_decay': 1e-6,  # Weight decay rate
+    'warming_epochs': 3,
+    'weight_decay': 1e-5,  # Weight decay rate
     'discr_params': {
         'activation': 'relu',
         'latent_space': LATENT_DIM,
-        'depth': 3,
+        # 'depth': 3,
         'encoding': 'both',
         'input_shape': SHAPE,
         'model': 'DiscriminatorXia',
@@ -49,7 +49,7 @@ EXPERIMENT_PARAMS = {
     'gen_params': {
         'activation': 'relu',
         'latent_space': LATENT_DIM,
-        'depth': 4,
+        # 'depth': 4,
         'encoding': 'both',
         'input_shape': SHAPE,
         'model': 'GeneratorXia',
