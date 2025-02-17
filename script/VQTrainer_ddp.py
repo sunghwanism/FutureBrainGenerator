ca6755ed835bc6c4bd58f505c253c881df992c57
SHMoon <msh2044@gmail.com>
2025-02-17 19:17:29 +0900

    remove the normalization

--- script/VQTrainer_ddp.py
+++ script/VQTrainer_ddp.py
@@ -68,10 +68,10 @@ def main(config):
             tio.RandomAffine(scales=(0.90, 1.1), p=0.5),
             tio.RandomAffine(degrees=(0, 10), p=0.5),
             tio.RandomAffine(translation=(5, 5, 5), p=0.5),
-            tio.RescaleIntensity(out_min_max=(0, 1)),
+            # tio.RescaleIntensity(out_min_max=(0, 1)),
             ])
-        val_transform = tio.Compose([
-            tio.RescaleIntensity(out_min_max=(0, 1)),])
+        # val_transform = tio.Compose([tio.RescaleIntensity(out_min_max=(0, 1)),])
+        
     else:
         train_transform = None
         val_transform = None
